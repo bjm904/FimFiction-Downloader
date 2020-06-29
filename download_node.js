cd9b20d1af9fd7bc44ee0e201459e882ebadcb18
Bryce Meyer <bjm904@gmail.com>
2020-06-29 15:31:32 -0700

    Commit

--- download_node.js
+++ download_node.js
@@ -1,76 +1,97 @@
 var http=require("http");
 var fs=require("fs");
 
-var folder="/fiction";
+var folder="/media/FimFictions/";
 
-var num=Number(process.argv[2]);
+var num=Number(process.argv[2])-1;
 var stopNum=Number(process.argv[3]);
 var total=0;
 var amtBad=0;
 var amtFail=0;
 var amtAbort=0;
 var stop=false;
-function go(){
-	if(stopNum&&num>=stopNum){
-		console.log("DONE! "+num+" "+stopNum);
+var folderExists=[];
+function go(currentNum){
+	if(stopNum&&currentNum>=stopNum){
+		console.log("DONE! "+currentNum+" "+stopNum);
 		return;
 	}
-	//process.stdout.write("\u001b[2J\u001b[0;0H");
-	//process.stdout.write(num+"\n\nStarting... \n"+stats());
+	if(currentNum<=topNum){
+		return;
+	}
+	topNum=currentNum;
+	process.stdout.write("\u001b[2J\u001b[0;0H");
+	process.stdout.write(currentNum+"\n\nStarting... \n"+stats());
 	var options={
 		host: "www.fimfiction.net",
 		port: 80,
-		path: "/download_story.php?story="+num+"&html",
-		num:num+0
+		path: "/download_story.php?story="+currentNum+"&html",
+		num:currentNum+0
 	}
 
 	var request = http.get(options, function(res){
 		total++;
 		
 		if(res.headers["content-disposition"]){
-			var name=res.headers["content-disposition"].replace('attachment; filename="', "").slice(0, -1);;
-			name=name.replace(/[\?\/\\:\*\"<>\|]/g, "").replace(".html", "-"+num+".html");
+			var name=res.headers["content-disposition"].replace('attachment; filename="', "").slice(0, -1);
+			name=name.replace(/[\?\/\\:\*\"<>\|]/g, "").replace(".html", "-"+currentNum+".html");
+			name=name.replace(/[^a-zA-Z0-9.?!,%'"@#$^&*()\-_=+~`;:{}|\\>< \/]/g, "\?");
+			var incFolder=(Math.floor(currentNum/2000)*2000)+"/";
+			if(!folderExists[incFolder]){
+				if(!fs.existsSync(folder+incFolder)){
+					fs.mkdirSync(folder+incFolder);
+				}
+				folderExists[incFolder]=true;
+			}
 			process.stdout.write("\u001b[2J\u001b[0;0H");
-			process.stdout.write(num+"\n"+name+"\nDownloading... \n"+stats());
-			var writeStream = fs.createWriteStream(folder+name);
-			res.pipe(writeStream);
-			
-			var timeout=setTimeout(function(){
+			process.stdout.write(currentNum+"\n"+name+"\nDownloading... \n"+stats());
+			if((folder+incFolder+name).length>220){
 				res.on('end', function(){});
-				fs.appendFile("failed", num+"\n", function(err){
+				fs.appendFile("failed", currentNum+"\n", function(err){
 					amtFail++;
 					contin("Fail");
-					num++;
 				});
-			}, 10*1000);
-			
-			res.on('end', function(){
-				clearTimeout(timeout);
-				writeStream.end();
-				contin("Good");
-				num++;
-			});
+				return;
+			} else{
+				var writeStream = fs.createWriteStream(folder+incFolder+name);
+				res.pipe(writeStream);
+				
+				var timeout=setTimeout(function(){
+					res.on('end', function(){});
+					fs.appendFile("failed", currentNum+"\n", function(err){
+						amtFail++;
+						contin("Fail");
+					});
+				}, 8*1000);
+				
+				res.on('end', function(){
+					clearTimeout(timeout);
+					writeStream.end();
+					contin("Good");
+				});
+			}
 		} else{
 			amtBad++;
 			contin("Bad");
-			num++;
 		}
 	});
 }
 function toggleStop(){
 	stop=true;
 }
+var topNum=-1;
 function contin(code){
-	//process.stdout.write("\u001b[2J\u001b[0;0H");
+	process.stdout.write("\u001b[2J\u001b[0;0H");
 	if(!stop){
-		go();
+		num++;
+		go(num);
 		//process.stdout.write(num+"\n"+code+"\nWaiting...\n"+stats());
 	}else{
 		process.stdout.write(num+"\n\nStopped!\n"+stats());
 	}
 }
 function stats(){
-	return "Bad: "+amtBad+"/"+total+" "+(Math.round((amtBad/total)*100))+"%      Failed: "+amtFail+"/"+total+" "+(Math.round((amtFail/total)*100))+"%      Aborted: "+amtAbort+"/"+total+" "+(Math.round((amtAbort/total)*100))+"%\n";
+	return "Good: "+(total-amtBad)+"/"+total+" "+(Math.round(((total-amtBad)/total)*100))+"%      Bad: "+amtBad+"/"+total+" "+(Math.round((amtBad/total)*100))+"%      Failed: "+amtFail+"/"+total+" "+(Math.round((amtFail/total)*100))+"%      Aborted: "+amtAbort+"/"+total+" "+(Math.round((amtAbort/total)*100))+"%\n";
 }
 
 process.stdin.resume();
